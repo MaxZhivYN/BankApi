492b09634bc0e3a5885e8c45d8f39d722c70640a
MaxZhivYN <maxzhiv19@gmail.com>
2021-10-11 10:15:54 +0300

    switch from xml mapping to class-annotations

--- src/main/java/com/sberbank/maxzhiv/bankapi/store/entities/AccountEntity.java
+++ src/main/java/com/sberbank/maxzhiv/bankapi/store/entities/AccountEntity.java
@@ -2,18 +2,24 @@ package com.sberbank.maxzhiv.bankapi.store.entities;
 
 import lombok.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.persistence.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name = "ACCOUNT")
 public class AccountEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
 
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "account_balance")
     private Double balance;
 }
--- src/main/java/com/sberbank/maxzhiv/bankapi/store/entities/CardEntity.java
+++ src/main/java/com/sberbank/maxzhiv/bankapi/store/entities/CardEntity.java
@@ -10,12 +10,21 @@ import javax.persistence.*;
 @Builder
 @Getter
 @Setter
+@Entity
+@Table(name = "CARD")
 public class CardEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
 
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "card_balance")
     private Double balance;
 
+    @ManyToOne
+    @JoinColumn(name = "account_id")
     private AccountEntity account;
 }
